91604bc7af25d8dd1ba702adc8ba87fe690077c9
Jeffdev <robotzurg@gmail.com>
2025-02-20 16:19:15 -0700

    Ping the server every 10 minutes
    To keep it active

--- cs-4900-backend/api/index.ts
+++ cs-4900-backend/api/index.ts
@@ -23,3 +23,19 @@ app.use('/', artistRouter);
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Ping the server every 10 minutes
+const url = `https://cs4900-637g.onrender.com/`;
+const interval = 10 * 60000; 
+
+function reloadWebsite() {
+  fetch(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
