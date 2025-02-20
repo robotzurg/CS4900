9d43e5a5d5d005ef452278c471afded62378ac40
Jeffdev <robotzurg@gmail.com>
2025-02-19 21:20:34 -0700

    Potentially fixed vercel issues

--- cs-4900-backend/api/index.ts
+++ cs-4900-backend/api/index.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import albumRouter from './routes/albumRoutes.ts';
-import artistRouter from './routes/artistsRoutes.ts';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import vercel from '@vercel/node';
+import albumRouter from "./routes/albumRoutes.ts";
+import artistRouter from "./routes/artistsRoutes.ts";
 
 dotenv.config();
 
@@ -12,14 +13,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Setup main routes
-app.get('/', async (req, res) => {
-  res.send('API v1.0');
+// Root API response
+app.get("/", (req, res) => {
+  res.send("API v1.0");
 });
 
-app.use('/', albumRouter);
-app.use('/', artistRouter);
+// Register routes
+app.use("/api/albums", albumRouter);
+app.use("/api/artists", artistRouter);
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export Express handler for Vercel
+export default (req: vercel.VercelRequest, res: vercel.VercelResponse) => {
+  return app(req, res);
+};
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
