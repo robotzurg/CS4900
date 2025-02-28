8bf84999fb6d770e6922393989424d2883a55528
Jeffdev <robotzurg@gmail.com>
2025-02-28 00:17:05 -0700

    Fixed API url

--- cs-4900/src/services/api.ts
+++ cs-4900/src/services/api.ts
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const fetchSong = async (slug: string) => {
     try {
-        const res = await fetch(`http://localhost:3000/api/songs/${slug}`);
+        const devApiUrl = process.env.VITE_API_DEV_URL
+        const apiUrl = process.env.VITE_API_URL;
+        const res = await fetch(`${apiUrl}/api/songs/${slug}`);
         return res.json();
     } catch (err) {
         return { error: `Error retrieving song: ${err}` };
