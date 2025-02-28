e1e292ae840bd6d1a5821c0599345f29a6ac69fb
Jefferson Turpin <robotzurg@gmail.com>
2025-02-28 14:19:16 -0700

    Worked on the song page more

--- cs-4900/src/pages/Song/Song.tsx
+++ cs-4900/src/pages/Song/Song.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { fetchBySlug } from "../../services/api";
 import { Container, Row, Col } from "react-bootstrap";
+import { Flex, RingProgress, Text, Stack } from '@mantine/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpotify, faSoundcloud, faApple, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import Helmet from 'react-helmet';
 
 import MainNavbar from "../../components/MainNavbar";
 import './Song.css';
@@ -35,26 +39,70 @@ function SongPage() {
     <div>
       <MainNavbar />
       <Container className="mt-4 main-container">
+        <Row className="p-3 page-header">
+          <h1>{song.name}</h1>
+          <h3>
+            {song.artists.map((artist: any, index: number) => (
+              <span key={index}>
+              <Link className="artist-link" to={`/artist/${artist.name}`}>
+                {artist.name}
+              </Link>
+              {index < song.artists.length - 1 && ', '}
+            </span>
+            ))}
+          </h3>
+        </Row>
         <Row className="d-flex p-3">
-          {/* Song Info Section (Left) */}
-          <Col lg={8}>
-            <h1>{song.name}</h1>
-            <h3>
-              <strong>By:</strong> {song.artists.map((artist: any, index: number) => (
+            <Col lg={3} className="d-flex flex-column align-items-center">
+            <p>
+              <strong>Release Date:</strong> {new Date(song.release_date).toLocaleDateString()}<br />
+              <strong>Genres:</strong> {song.genres.map((genre: any, index: number) => (
                 <span key={index}>
-                <Link className="artist-link" to={`/artist/${artist.name}`}>
-                  {artist.name}
+                <Link className="genre-link" to={`/genre/${genre.name}`}>
+                  {genre.name}
                 </Link>
                 {index < song.artists.length - 1 && ', '}
               </span>
               ))}
-            </h3>
-            <p>
-              <strong>Release Date:</strong> {new Date(song.release_date).toLocaleDateString()}
             </p>
+            <Flex gap="5">
+              <FontAwesomeIcon icon={faSpotify} size="2x" fixedWidth />
+              <FontAwesomeIcon icon={faSoundcloud} size="2x" fixedWidth />
+              <FontAwesomeIcon icon={faApple} size="2x" fixedWidth />
+              <FontAwesomeIcon icon={faYoutube} size="2x" fixedWidth />
+            </Flex>
           </Col>
 
-          {/* Song Image Section (Right) */}
+          <Col lg={5} className="d-flex flex-column align-items-center">
+            <h2>Ratings</h2>
+            <Flex gap="10">
+              <Stack gap="0">
+                <RingProgress
+                  size={110}
+                  sections={[{ value: 90, color: 'blue' }]}
+                  label={<Text ta="center">9.0</Text>}
+                />
+                <Text ta="center">Critic</Text>
+              </Stack>
+              <Stack gap="0" align="center">
+                <RingProgress
+                  size={110}
+                  sections={[{ value: 82, color: 'red' }]}
+                  label={<Text ta="center">8.2</Text>}
+                />
+                <Text ta="center">User</Text>
+              </Stack>
+              <Stack gap="0">
+                <RingProgress
+                  size={110}
+                  sections={[{ value: 75, color: 'green' }]}
+                  label={<Text ta="center">7.5</Text>}
+                />
+                <Text ta="center">Friends</Text>
+              </Stack>
+            </Flex>
+          </Col>
+
           <Col lg={4} className="text-center">
             <img
               src={song.image_url}
--- cs-4900/src/services/api.ts
+++ cs-4900/src/services/api.ts
@@ -1,8 +1,10 @@
-const apiUrl = import.meta.env.VITE_API_DEV_URL;
+const isDev = true;
+const apiUrl = import.meta.env.VITE_API_URL;
+const apiDevUrl = import.meta.env.VITE_API_DEV_URL;
 
 export const fetchBySlug = async (entity: string, slug: string) => {
     try {
-        const res = await fetch(`${apiUrl}/api/${entity}/${slug}`);
+        const res = await fetch(`${isDev ? apiUrl : apiDevUrl}/api/${entity}/${slug}`);
         return res.json();
     } catch (err) {
         return { error: `Error retrieving ${entity}: ${err}` };
