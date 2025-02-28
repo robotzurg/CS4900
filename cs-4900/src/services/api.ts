6abe98c5c022dce398956b5445de4691cc9dfa57
Jeffdev <robotzurg@gmail.com>
2025-02-28 00:17:16 -0700

    Update api.ts

--- cs-4900/src/services/api.ts
+++ cs-4900/src/services/api.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const fetchSong = async (slug: string) => {
     try {
-        const devApiUrl = process.env.VITE_API_DEV_URL
+        //const devApiUrl = process.env.VITE_API_DEV_URL
         const apiUrl = process.env.VITE_API_URL;
         const res = await fetch(`${apiUrl}/api/songs/${slug}`);
         return res.json();
