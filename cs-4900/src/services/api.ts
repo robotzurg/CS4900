9b60ed3f2e473d02bf4da653e9ff0425be69db1f
Jeffdev <robotzurg@gmail.com>
2025-03-19 18:17:12 -0600

    I'm praying that maybe this fixes it

--- cs-4900/src/services/api.ts
+++ cs-4900/src/services/api.ts
@@ -1,9 +1,9 @@
-const isDev = import.meta.env.VITE_IS_DEV;
+let isDev = import.meta.env.VITE_IS_DEV;
 const apiUrl = import.meta.env.VITE_API_URL;
 const apiDevUrl = import.meta.env.VITE_API_DEV_URL;
 
 export const authLogin = () => {
-    window.location.href = `${isDev === 'true' ? apiDevUrl : apiUrl}/login/federated/google`;
+    window.location.href = `${isDev === 'true' ? apiDevUrl : apiUrl}/login/federated/google/`;
 };
 
 export const authLogout = async () => {
