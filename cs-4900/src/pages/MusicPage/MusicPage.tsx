6835644bbeed7cc76e6460eb3dd8744582a8a50f
Jeffdev <robotzurg@gmail.com>
2025-04-09 02:42:03 -0600

    Update MusicPage.tsx

--- cs-4900/src/pages/MusicPage/MusicPage.tsx
+++ cs-4900/src/pages/MusicPage/MusicPage.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { fetchById, getMusicReviews } from "../../services";
 import MusicInfoCard from "../../components/MusicInfoCard";
-import { Col, Row } from "react-bootstrap";
 
 function MusicPage({ entity }: { entity: string }) {
   const { musicId } = useParams(); 
